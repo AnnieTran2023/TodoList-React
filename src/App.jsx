bab803861c9d599c60ecb121c7e00ebc7cd92862
Annie Tran <tranthilananh105@gmail.com>
2024-10-01 10:00:37 +0300

    add mui and styling for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import "./App.css";
 import Todolist from "./components/Todolist";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 function App() {
-
   return (
-    <>
-      <Todolist />
-    </>
+    <Container maxWidth="lg">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">My Todos</Typography>
+        </Toolbar>
+      </AppBar>
+      <Box mt={2}>
+        <Todolist />
+      </Box>
+      <CssBaseline />
+    </Container>
   );
 }
 
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,9 +1,13 @@
 import { useState, useRef } from "react";
-
 import { AgGridReact } from "ag-grid-react";
-
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css"; // Material Design theme
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import Stack from "@mui/material/Stack";
 
 export default function Todolist() {
   const [todo, setTodo] = useState({ desc: "", duedate: "", priority: "Low" });
@@ -32,7 +36,7 @@ export default function Todolist() {
       setTodo({ desc: "", duedate: "", priority: "" });
     }
   };
-  
+
   const handleDelete = () => {
     if (gridRef.current.getSelectedNodes().length > 0) {
       setTodos(
@@ -47,51 +51,52 @@ export default function Todolist() {
 
   return (
     <>
-      <h3 style={{ background: "black", color: "white" }}>Simple Todolist</h3>
-      <span>Description:</span>
-      <input
-        type="text"
-        placeholder="Type todo here..."
-        onChange={(event) => setTodo({ ...todo, desc: event.target.value })}
-        value={todo.desc}
-        style={{
-          height: 35,
-          marginRight: 20,
-          marginLeft: 5,
-        }}
-      />
-      <label htmlFor="priority" style={{ marginRight: 10 }}>
-        Priority:
-      </label>
-      <select
-        id="priority"
-        onChange={(event) => setTodo({ ...todo, priority: event.target.value })}
-        value={todo.priority}
-        style={{
-          height: 35,
-          marginRight: 20,
-          marginLeft: 5,
-        }}
+      <Stack
+        direction="row"
+        spacing={3}
+        justifyContent="center"
       >
-        <option value="Low">Low</option>
-        <option value="Medium">Medium</option>
-        <option value="High">High</option>
-      </select>
-      <span>Due date:</span>
-      <input
-        type="date"
-        placeholder="Type due date..."
-        onChange={(event) => setTodo({ ...todo, duedate: event.target.value })}
-        value={todo.duedate}
-        style={{
-          height: 35,
-          marginRight: 20,
-          marginLeft: 5,
-        }}
-      />
-      <button onClick={addTodo}>Add</button>
-      <button onClick={handleDelete}>Delete</button>
-      <div className="ag-theme-material" style={{ width: 700, height: 500 }}>
+        <TextField
+          type="text"
+          label="Enter todo"
+          onChange={(event) => setTodo({ ...todo, desc: event.target.value })}
+          value={todo.desc}
+          variant="outlined"
+        />
+        <TextField
+          select
+          label="Priority"
+          value={todo.priority}
+          onChange={(event) =>
+            setTodo({ ...todo, priority: event.target.value })
+          }
+          variant="outlined"
+        >
+          <MenuItem value="Low">Low</MenuItem>
+          <MenuItem value="Medium">Medium</MenuItem>
+          <MenuItem value="High">High</MenuItem>
+        </TextField>
+        <TextField
+          label="Date"
+          variant="outlined"
+          onChange={(event) =>
+            setTodo({ ...todo, duedate: event.target.value })
+          }
+          value={todo.duedate}
+        />
+        <Button variant="outlined" endIcon={<AddIcon />} onClick={addTodo}>
+          Add
+        </Button>
+        <Button
+          variant="outlined"
+          color="error"
+          endIcon={<DeleteIcon />}
+          onClick={handleDelete}
+        >
+          Delete
+        </Button>
+      </Stack>
+      <div className="ag-theme-material" style={{ width: "100%", height: 500 }}>
         <AgGridReact
           ref={gridRef}
           onGridReady={(params) => (gridRef.current = params.api)}
